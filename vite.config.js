2837a3e9e5796db5f9ca321b4c60faadc6d5b544
tsehla42 <tsehla@duck.com>
2026-04-06 14:38:15 +0300

    chore: scaffold Vite + Vitest project

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
+
+export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    minify: false,
+  },
+  plugins: [viteSingleFile()],
+});
